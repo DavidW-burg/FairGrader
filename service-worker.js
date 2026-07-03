4929c29b4a344a4ddde1dfe1c5a225d7187722a2
DavidW-burg <DavidW-burg@users.noreply.github.com>
2026-07-03 13:34:53 +0000

    deploy: dbcbc3a3cb47cd7df2f6380c9a440c712a264c09

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+﻿// Nur für Entwicklung – tut absichtlich nichts
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
+self.addEventListener('fetch', () => { });
